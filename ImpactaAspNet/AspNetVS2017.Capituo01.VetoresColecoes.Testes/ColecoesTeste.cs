af5bf63e7def55423d91d6965efa00a4afb65c9d
Lucas de Oliveira Gomes <xzzlucaszzx@gmail.com>
2018-11-23 22:22:40 -0200

    Added ColecoesTeste and Changed VetoresTeste

---
+++ ImpactaAspNet/AspNetVS2017.Capituo01.VetoresColecoes.Testes/ColecoesTeste.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AspNetVS2017.Capituo01.VetoresColecoes.Testes
+{
+    [TestClass]
+    public class ColecoesTeste
+    {
+        [TestMethod]
+        public void ListTeste()
+        {
+            var inteiros = new List<int>(50) { 2, 1, 2, 6, 125 };
+            inteiros.Add(22);
+            //inteiros[12] = 89;
+
+            var maisInteiros = new List<int> { 21, 72, 6, 14 };
+
+            inteiros.AddRange(maisInteiros);
+            //inteiros.Insert(0, 29); //Se o índice não existe vai dar Exceção
+
+            inteiros.Remove(2);
+            //inteiros.RemoveAt(189); //Se o índice não existe vai dar Exceção
+
+            inteiros.Sort();
+
+            var primeiro = inteiros[0];
+            //var primeiro = inteiros.First() //Requer Linq dá erro se a Lista estiver vazia
+            //var primeiro = inteiros.FirstOrDefault() //Requer Linq dá erro se a Lista estiver vazia
+
+            var utilmo = inteiros[inteiros.Count - 1];
+            var estahVazia = inteiros.Count == 0;
+
+            foreach (var inteiro in inteiros)
+            {
+                Console.WriteLine($"{inteiros.IndexOf(inteiro)}:{inteiro}");
+            }
+            Console.WriteLine(new string('-', 30));
+            for (int i = 0; i < inteiros.Count; i++)
+            {
+                Console.WriteLine($"{i}:{inteiros[i]}");
+            }
+        }
+
+        [TestMethod]
+        public void DictionaryTeste()
+        {
+            var feriados = new Dictionary<DateTime, string>();
+            feriados.Add(new DateTime(2018, 12, 25), "Natal");
+            feriados.Add(new DateTime(2019, 1, 1), "Ano Novo");
+            feriados.Add(new DateTime(2019, 1, 25), "Aniversário São Paulo");
+            //feriados.Add(new DateTime(2019, 1, 25), "Aniversário da Fulaninha");
+
+            var natal = feriados[new DateTime(2018, 12, 25)];
+
+            foreach (var feriado in feriados)
+            {
+                //Console.WriteLine($"{feriado.Key.ToShortDateString()} é {feriado.Value}");
+                //Console.WriteLine(string.Format("{0:dd/MM} é {1}", feriado.Key, feriado.Value));
+                Console.WriteLine(string.Format("{0} é {1}", feriado.Key.ToString("dd/MM"), feriado.Value));
+            }
+
+            Console.WriteLine(feriados.ContainsKey(Convert.ToDateTime("25/12/2018")));
+            Console.WriteLine(feriados.ContainsValue("Ano Novo"));
+        }
+    }
+}
---
+++ ImpactaAspNet/AspNetVS2017.Capituo01.VetoresColecoes.Testes/VetoresTeste.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AspNetVS2017.Capituo01.VetoresColecoes.Testes
+{
+    [TestClass]
+    public class VetoresTeste
+    {
+        [TestMethod]
+        public void InicializacaoTeste()
+        {
+            var strings = new string[10];
+            strings[0] = "A";
+            //strings[-1] = "Outra coisa"; //Erro de execução (matrizes iniciam a partir do 0)
+            //strings[10] = "B"; // Erro de execução (o tamanho informado não é alcançavel)
+            var decimais = new decimal[] { 0.5m, 78, 1.59m };
+
+            decimal[] novosDecimais = { 0.99m, 5, 45.646m }; // não pode user var
+
+            foreach (var @decimal in decimais)
+            {
+                Console.WriteLine(@decimal);
+            }
+            Console.WriteLine($"Tamanho do vetor: {decimais.Length}");
+        }
+
+        [TestMethod]
+        public void RedimensionamentoTeste()
+        {
+            var decimais = new decimal[] { 0.5m, 78, 1.59m };
+            Array.Resize(ref decimais, 5);
+
+            decimais[3] = 20.01m;
+        }
+
+        [TestMethod]
+        public void OrdenacaodorTeste()
+        {
+            var decimais = new decimal[] { 35, 89m, 0.5m, 78, 1.59m };
+            Array.Sort(decimais);
+
+            Assert.AreEqual(decimais[0], 0.5m);
+        }
+
+        [TestMethod]
+        public void MediaTeste()
+        {
+            decimal[] valores = { 0.4m, 1.6m, 0.8m, 0.3m };
+
+            var media = ObterMedia(valores);
+            Console.WriteLine(media);
+        }
+
+        [TestMethod]
+        public void ParamsTeste()
+        {
+            var decimais = new decimal[] { 35.89m, 0.5m, 78, 1.59m };
+            Console.WriteLine(ObterMedia(decimais));
+            Console.WriteLine(ObterMedia(2, 8, 9.8m, 4.23m));
+        }
+
+        [TestMethod]
+        public void TodaStringEhUmVetorTeste()
+        {
+            const string nome = "Hejlsberg";
+            Assert.AreEqual(nome[0], 'H');
+            foreach (var @char in nome)
+            {
+                Console.WriteLine(@char);
+            }
+        }
+
+        private decimal ObterMedia(decimal num1, decimal num2)
+        {
+            decimal media = 0;
+            media = (num1 + num2) / 2;
+            return media;
+        }
+
+        private decimal ObterMedia(params decimal[] decimais)
+        {
+            //return decimais.Average();
+            decimal media = 0;
+            var qtdItens = decimais.Length;
+            decimal total = 0;
+
+            foreach (var @decimal in decimais)
+            {
+                total += @decimal;
+            }
+            media = total / qtdItens;
+            return media;
+        }
+    }
+}
