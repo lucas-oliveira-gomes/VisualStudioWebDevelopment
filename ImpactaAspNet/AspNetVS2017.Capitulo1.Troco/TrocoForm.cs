8f0ea2d4bfd11a733fb6cfd5026ce17dc3cecee0
Lucas de Oliveira Gomes <xzzlucaszzx@gmail.com>
2018-11-26 20:04:34 -0200

    Refactoring Troco

--- ImpactaAspNet/AspNetVS2017.Capitulo1.Troco/TrocoForm.cs
+++ ImpactaAspNet/AspNetVS2017.Capitulo1.Troco/TrocoForm.cs
@@ -21,40 +21,19 @@ namespace AspNetVS2017.Capitulo1.Troco
         {
             decimal valorDaCompra = 0m;
             valorDaCompra = Convert.ToDecimal(valorCompraTextBox.Text);
-
             decimal valorPago = 0m;
             valorPago = Convert.ToDecimal(valorPagoTextBox.Text);
             //valorPago = decimal.Parse(valorPagoTextBox.Text);
-
             decimal troco = valorPago - valorDaCompra;
-
             TrocoTextBox.Text = troco.ToString("C2");
 
-            //TODO: Refatorar para usar vetor com foreach
-            var moedas1 = (int)(troco / 1);
-            troco %= 1;
+            var moedas = new decimal[] { 1, 0.5m, 0.25m, 0.1m, 0.05m, 0.01m };
 
-            var moedas050 = (int)(troco / 0.5m);
-            troco %= 0.5m;
-
-            var moedas025 = (int)(troco / 0.25m);
-            troco %= 0.25m;
-
-            var moedas010 = (int)(troco / 0.10m);
-            troco %= 0.10m;
-
-            var moedas005 = (int)(troco / 0.05m);
-            troco %= 0.05m;
-
-            var moedas001 = (int)(troco / 0.01m);
-            troco %= 0.01m;
-
-            moedasListView.Items[0].Text = moedas1.ToString();
-            moedasListView.Items[1].Text = moedas050.ToString();
-            moedasListView.Items[2].Text = moedas025.ToString();
-            moedasListView.Items[3].Text = moedas010.ToString();
-            moedasListView.Items[4].Text = moedas005.ToString();
-            moedasListView.Items[5].Text = moedas001.ToString();
+            for (int i = 0; i < moedas.Length; i++)
+            {
+                moedasListView.Items[i].Text = ((int)(troco / moedas[i])).ToString();
+                troco %= moedas[i];
+            }
         }
     }
 }
