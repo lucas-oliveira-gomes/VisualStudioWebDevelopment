440ca275ca2f669774174cd4383ea1a9bd0d91ae
Lucas de Oliveira Gomes <xzzlucaszzx@gmail.com>
2018-12-06 21:31:58 -0200

    Adding new Project MVC

--- ImpactaAspNet/ViagensOnLine.Respositorios.SqlServer/ViagensOnlineDbContext.cs
+++ ImpactaAspNet/ViagensOnLine.Respositorios.SqlServer/ViagensOnlineDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,5 +17,11 @@ namespace ViagensOnLine.Respositorios.SqlServer
         }
 
         public DbSet<Destino> Destinos { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //base.OnModelCreating(modelBuilder);
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
     }
 }
--- ImpactaAspNet/ViagensOnLine.Respositorios.SqlServerTests/ViagensOnlineDbContextTests.cs
+++ ImpactaAspNet/ViagensOnLine.Respositorios.SqlServerTests/ViagensOnlineDbContextTests.cs
@@ -12,9 +12,45 @@ namespace ViagensOnLine.Respositorios.SqlServer.Tests
     [TestClass()]
     public class ViagensOnlineDbContextTests
     {
+        private readonly ViagensOnlineDbContext db = new ViagensOnlineDbContext();
+
         [TestMethod()]
         public void InserirTest()
         {
+            var destino = new Destino
+            {
+                Cidade = "Victoria",
+                Nome = "Victoria",
+                Pais = "Canadá",
+                NomeImagem = "victoria_canada.png"
+            };
+
+            db.Destinos.Add(destino);
+            db.SaveChanges();
+        }
+
+        [TestMethod]
+        public void AtualizarTeste()
+        {
+            var destino = db.Destinos.Find(1);
+            destino.Cidade = "Quebec";
+            destino.Nome = "Quebec";
+
+            db.SaveChanges();
+
+            var destino2 = db.Destinos.Find(1);
+
+            Assert.AreEqual(destino2.Cidade, "Quebec");
+        }
+
+        [TestMethod]
+        public void ExcluirTeste()
+        {
+            var destino = db.Destinos.Find(1);
+            db.Destinos.Remove(destino);
+            db.SaveChanges();
+            destino = db.Destinos.Find(1);
+            Assert.IsNull(destino);
         }
     }
 }
