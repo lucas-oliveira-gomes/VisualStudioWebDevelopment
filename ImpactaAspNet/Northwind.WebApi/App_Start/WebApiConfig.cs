41f3d4cc59f2d846a810f65f01416a2a9cb0a648
Lucas de Oliveira Gomes <xzzlucaszzx@gmail.com>
2018-12-17 22:12:12 -0200

    Working with Web API

---
+++ ImpactaAspNet/Northwind.WebApi/App_Start/WebApiConfig.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Web.Http;
+
+namespace Northwind.WebApi
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Serviços e configuração da API da Web
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            // Rotas da API da Web
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
